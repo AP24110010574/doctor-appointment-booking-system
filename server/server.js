22cf808de27cedb042d74427d2e672bcb7c78345
kanishk <ravipatikanishk@gmail.com>
2026-04-18 12:35:10 +0530

    update api url for production

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://docbook-ap.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
